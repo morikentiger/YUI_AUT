6e1d58ba85db78fd158cb4bdefa1f4c4cd528606
morikentiger <kenblacksword339@gmail.com>
2023-10-10 01:55:33 +0900

    Initial Commit

---
+++ YUI_AUT/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YUI_AUT
+//
+//  Created by 森田健太 on 2023/10/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YUI_AUT/YUI_AUTApp.swift
@@ -0,0 +1,17 @@
+//
+//  YUI_AUTApp.swift
+//  YUI_AUT
+//
+//  Created by 森田健太 on 2023/10/10.
+//
+
+import SwiftUI
+
+@main
+struct YUI_AUTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
